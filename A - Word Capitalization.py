c306797e333ab487725be255635adcfe9df159e2
David Felipe Bautista <78710558+BautistaDavid@users.noreply.github.com>
2023-01-05 23:14:59 -0500

    Create A - Word Capitalization.py

---
+++ A - Word Capitalization.py
@@ -0,0 +1,7 @@
+# A - Word Capitalization - By: David Bautista - @PipeBau
+# url: https://codeforces.com/contest/281/problem/A
+
+w = input()
+w_1 = w[0]
+w_2 = w[1:len(w)]
+print(w_1.title() + w_2)
